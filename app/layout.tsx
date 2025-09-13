102ef059917476535f76bd9c16c9b8c56ffaeb0b
Blackmamoth <blackmamoth83@gmail.com>
2025-09-13 23:19:09 +0530

    feat: add auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
 import { Suspense } from "react"
 import "./globals.css"
+import { Toaster } from "@/components/ui/sonner"
 
 export const metadata: Metadata = {
 	title: "Envyron - Standardize your environment",
@@ -22,6 +23,7 @@ export default function RootLayout({
 			<body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}>
 				<Suspense fallback={null}>{children}</Suspense>
 				<Analytics />
+				<Toaster />
 			</body>
 		</html>
 	)
---
+++ db/index.ts
@@ -0,0 +1,15 @@
+import { env } from "@/lib/env";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+  max: env.MAX_DATABASE_CONNECTIONS,
+  min: env.MIN_DATABASE_CONNECTIONS,
+  idleTimeoutMillis: env.IDLE_TIMEOUT_IN_SECONDS * 1000,
+  connectionTimeoutMillis: env.CONNECTION_TIMEOUT_IN_SECONDS * 1000,
+  keepAlive: true,
+});
+
+export const db = drizzle({ client: pool });
